92c5b08e5a30ea2131bddb363ee52673a01af118
Alisha Ukani <alishaaukani@gmail.com>
2014-07-02 11:34:01 -0400

    Put styling in style.php

--- index.php
+++ index.php
@@ -3,9 +3,10 @@
 	<head>
 		<title>Clock</title>
 		<?php require 'logic.php' ?>
+		<link rel="stylesheet" type="text/css" href="style.php">
 	</head>
 
-	<body style="background-color:<?php echo $background ?>">
+	<body>
 		<h1>It is <?php echo date('g:i a') ?></h1>
 		<img src='http://making-the-internet.s3.amazonaws.com/php-<?php echo $image ?>'>
 	</body>
---
+++ style.php
@@ -0,0 +1,14 @@
+<?php
+	header("Content-type: text/css; charset: UTF-8");
+	require 'logic.php';
+?>
+
+body {
+	background-color: <?php echo $background ?>
+}
+
+h1 {
+	font-family: 'Helvetica';
+	color: white;
+	text-align: center;
+}
