ffb709ea236df2396e8abb51e224f0c4caa5d06b
Alisha Ukani <alishaaukani@gmail.com>
2014-07-02 11:27:29 -0400

    Change background color depending on time

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 		<?php require 'logic.php' ?>
 	</head>
 
-	<body>
+	<body style="background-color:<?php echo $background ?>">
 		<h1>It is <?php echo date('g:i a') ?></h1>
 		<img src='http://making-the-internet.s3.amazonaws.com/php-<?php echo $image ?>'>
 	</body>
--- logic.php
+++ logic.php
@@ -4,10 +4,14 @@ $hour = intval(date('G'));
 
 if ($hour >= 5 && $hour < 11) {
 	$image = 'morning.png';
+	$background = '#865f86';
 } elseif ($hour >= 11 && $hour < 16) {
 	$image = 'afternoon.png';
+	$background = '#2c87c8';
 } elseif ($hour >= 16 && $hour < 20) {
 	$image = 'evening.png';
+	$background = '#c7b02f';
 } else {
 	$image = 'night.png';
+	$background = '#180629';
 }
